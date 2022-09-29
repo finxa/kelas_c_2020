c791a7ba45a3f788d392c2188aa0e3cadca038c0
IcaQrtl <ichaqurotulaeni22@gmail.com>
2022-09-29 19:49:18 +0700

    Latihan Bootstrep

---
+++ bootstrap/5520120107_IcaQurotulAeni.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ica qurotul aeni</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="container">
+    <h1>ANNYOENG YOEROBUN</h1>
+    <div class="text-center">
+    <h1 class="container" style="background: mediumturquoise;">ICA QUROTUL AENI</h1>
+  </div>
+      <div class="text-center">
+      <div class="row">
+          <div class=" col-6 col-md-3" style="background:Plum;">
+              <div>Park Chanyoel</div>
+          </div>
+          <div class="col-6 col-md-3" style="background:Pink;">
+            <div>Zhang Yixing</div>
+        </div>
+        <div class="col-6 col-md-3" style="background:skyblue;">
+          <div>Do Kyunsoo</div>
+      </div>
+      <div class="col-6 col-md-3" style="background:yellowgreen;">
+        <div>Nakamoto Yuta</div>
+      </div>
+      </div>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th style="background: yellowgreen;">Kolom</th>
+                    <th  style="background: Pink;">Kolom</th>
+                    <th  style="background: skyblue;">Kolom</th>
+                    <th  style="background: yellowgreen;">Kolom</th>
+                    <th  style="background: pink;">Kolom</th>
+                    <th  style="background: skyblue;">Kolom</th>
+                    <th style="background:plum;">Kolom</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                </tr>
+                <tr>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                </tr>
+                <tr>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                </tr>
+                <tr>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                    <td>Contoh Kolom</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <form>
+            <div class="mb-3 col-md-6">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3 col-md-6">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+        </form>
+
+        <form class="row g-3">
+            <div class="col-auto">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="col-auto">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+        </form>
+    </div>
+    <form class="form-horizontal">
+        <div class="mb-3 row">
+            <label for="exampleInputEmail1" class="form-label col-md-2">Email address</label>
+            <div class="col-md-8">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3 row">
+            <label for="exampleInputPassword1" class="form-label col-md-2">Password</label>
+            <div class="col-md-8">
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+        </div>
+    </form>
+
+    <select class="form-control">
+        <option>One</option>
+        <option>Two</option>
+        <option>Three</option>
+    </select>
+    <textarea class="form-cotnrol" rows="3"> </textarea>
+
+</div>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
